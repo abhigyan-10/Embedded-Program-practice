2abcbea56e03c44418eab1bd6feb498234cba789
Abhigyan Roy <163134341+abhigyan-10@users.noreply.github.com>
2026-02-23 18:25:29 +0530

    Update sketch.ino

--- Controlling LEDS/blinking_leds2/sketch.ino
+++ Controlling LEDS/blinking_leds2/sketch.ino
@@ -12,7 +12,7 @@ void loop() {
   volatile char s=0;
   volatile char i;
   for(i=0;i<=7;i++){
-    s= s|(1<<i);
+    s= s|(1<<i); //bit masking.....to update and store the values
     output(s);
     for(x=0;x<=300000;x++);
   }
