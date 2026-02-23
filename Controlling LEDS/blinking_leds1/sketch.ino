de72d703b0483b186b2c543c3b9c656b716db904
Abhigyan Roy <163134341+abhigyan-10@users.noreply.github.com>
2026-02-23 17:55:29 +0530

    Update sketch.ino

--- Controlling LEDS/blinking_leds1/sketch.ino
+++ Controlling LEDS/blinking_leds1/sketch.ino
@@ -8,10 +8,11 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   volatile long x;
-  char i;
+  char s;
+  volatile char i;
   for(i=0;i<=7;i++){
-    x=1<<i;
-    output(x);
+    s=1<<i;
+    output(s);
     for(x=0;x<=60000;x++);
   }
 }
