0e1aa8309d2e21fc1faf2c014272f15f22847421
Abhigyan Roy <163134341+abhigyan-10@users.noreply.github.com>
2026-02-23 13:10:34 +0530

    Create led.h

---
+++ Controlling LEDS/odd position/led.h
@@ -0,0 +1,12 @@
+void init();
+void output(char x);
+
+void init(){
+  volatile char *PORT_dir = (char *)0x107;
+  *PORT_dir = 0xFF;
+}
+
+void output(char x){
+  volatile char *out = (char *)0x108;
+  *out = x;
+}
