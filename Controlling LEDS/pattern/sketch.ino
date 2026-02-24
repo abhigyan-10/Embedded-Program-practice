0d7526452191342e82a8ffe82cebda41a0917f51
Abhigyan Roy <163134341+abhigyan-10@users.noreply.github.com>
2026-02-25 01:04:09 +0530

    Update sketch.ino

--- Controlling LEDS/pattern/sketch.ino
+++ Controlling LEDS/pattern/sketch.ino
@@ -5,19 +5,30 @@ void setup() {
   // put your setup code here, to run once:
   init();
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
   volatile long x;
-  volatile unsigned char m=0x01;
-  volatile unsigned char n=0x10;
+  volatile unsigned char s;
+  volatile unsigned char p[]={0,2,1,3,4,6,5,7};
   volatile char i;
-  for(i=0;i<2;i++){
-    pat(&m);
+  for(i=0;i<8;i++){
+      s=(1<<1)-1;
+      output(s<<p[i]);
+      for(x=0;x<600000;x++); 
   }
-  for(i=0;i<2;i++){
-    pat(&n);
+}
+// void loop() {
+//   // put your main code here, to run repeatedly:
+//   volatile long x;
+//   volatile unsigned char m=0x01;
+//   volatile unsigned char n=0x10;
+//   volatile char i;
+//   for(i=0;i<2;i++){
+//     pat(&m);
+//   }
+//   for(i=0;i<2;i++){
+//     pat(&n);
     
-  }
-}}
+//   }
+// }
 }
