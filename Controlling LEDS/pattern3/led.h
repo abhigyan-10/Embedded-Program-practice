16a226d24785bae71324d7d46092fb00093506dd
Abhigyan Roy <163134341+abhigyan-10@users.noreply.github.com>
2026-02-25 00:09:39 +0530

    Create led.h

---
+++ Controlling LEDS/pattern3/led.h
@@ -0,0 +1,17 @@
+#ifndef LED_H
+#define LED_H
+
+void init();
+void output(char x);
+
+void init(){
+  volatile char *PORT_dir = (char *)0x21;
+  *PORT_dir = 0xFF;
+}
+
+void output(char x){
+  volatile char *out = (char *)0x22;
+  *out = x;
+}
+
+#endif
