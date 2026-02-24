c4328b9dd27880ac4554899e24e975d9c707df2f
Abhigyan Roy <163134341+abhigyan-10@users.noreply.github.com>
2026-02-24 23:26:48 +0530

    Create sketch.ino

---
+++ Controlling LEDS/pattern2/sketch.ino
@@ -0,0 +1,24 @@
+// following pattern in pair (0,1) -> (6,7) -> (2,3) -> (4,5)
+#include "led.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  init();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  volatile long x;
+  volatile unsigned char s=0x03;
+  volatile unsigned char n=0xC0;
+  volatile char i;
+  for(i=0;i<2;i++){
+      
+      output(s);
+      for(x=0;x<500000;x++); 
+      output(n);
+      for(x=0;x<500000;x++); 
+      s = s << 2;
+      n = n >> 2;
+  }
+}
