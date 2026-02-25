e02a09a880589e35169d58085e41a0c1735ebd1d
Abhigyan Roy <163134341+abhigyan-10@users.noreply.github.com>
2026-02-25 19:05:47 +0530

    Create led.h

---
+++ Controlling LEDS/two_ports/led.h
@@ -0,0 +1,21 @@
+//port A and port B
+void output(char x);
+void output1(char x);
+void init(void);
+
+void init(void){
+  volatile char *PORTA_dir = (char *)0X21;
+  *PORTA_dir = 0xFF;
+  volatile char *PORTB_dir = (char *)0x24;
+  *PORTB_dir = 0xFF;
+}
+
+void output(char x){
+  volatile char *output = (char *)0X22;
+  *output = x;
+}
+
+void output1(char x){
+  volatile char *output = (char *)0x25;
+  *output = x;
+}
